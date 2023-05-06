824856b4aef9c15d6a7a47677286a530d83ddab1
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-06 08:49:37 +0300

    completed lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+
+  if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+    else
+
+      items = Item.all
+  end
+
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(save_params)
+    render json: item, except: [:created_at, :updated_at], status: :created
+
+  end
+  private
+
+  def save_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+  def render_not_found_response
+    render json: { error: "Restaurant not found" }, status: :not_found
+  end
 end
